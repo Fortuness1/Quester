d26c380fa548a84d97ca7e58c4d6c188e60bab27
Davi C. Pes. <davicalo2d@gmail.com>
2024-09-25 19:08:46 -0300

    adjustment student report

---
+++ frontend/src/scripts/alunoHome.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", async function () {
+    const userId = localStorage.getItem("userId");
+    const occupation = localStorage.getItem("occupation");
+
+    const setphoto = await getPhoto(userId);
+
+    if (userId && occupation) {
+        try {
+            // Fazer uma requisição para buscar as informações do usuário pelo userId
+            const response = await fetch("http://localhost:3000/user", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ _id: userId }),
+            });
+            if (response.ok) {
+                const userData = await response.json();
+                // Exibir as informações do usuário na tela
+                document.getElementById(
+                    "userName"
+                ).textContent = `${userData.surname}`;
+            } else {
+                // Se o usuário não for encontrado ou ocorrer um erro, redirecionar para a página de login
+                localStorage.clear()
+                window.location.href = "../../index.html";
+            }
+        } catch (error) {
+            console.error("Erro ao buscar informações do usuário:", error);
+            localStorage.clear()
+            window.location.href = "../../index.html"; // Redireciona em caso de erro
+        }
+    } else {
+        // Redireciona para a página de login se não houver informações no localStorage
+        localStorage.clear()
+        window.location.href = "../../index.html";
+    }
+
+    const logoutButton = document.getElementById("logout");
+    logoutButton.addEventListener("click", function () {
+        localStorage.removeItem("userId");
+        localStorage.removeItem("occupation");
+    });
+
+    const buttonPin = document.getElementById("button-pin");
+    buttonPin.addEventListener("click", async function () {
+        console.error("Erro na requisição:");
+        const pinInput = document.getElementById("pin");
+        const pin = pinInput.value;
+
+        if (!pin) {
+            return;
+        }
+
+        try {
+            const response = await fetch(`http://localhost:3000/match/enter/${pin}`, {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                localStorage.setItem("matchId", data.id_match);
+                localStorage.setItem("matchRole", "PLAYER");
+                window.location.href = "./MatchScreen.html";
+            } else {
+                console.error("Erro na requisição:", error);
+                pinInput.style.border = "#ff0000 2px solid"
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            pinInput.style.border = "#ff0000 2px solid"
+        }
+    });
+});
+
+const getPhoto = async (idUSer) => {
+    try {
+        const response = await fetch(`http://localhost:3000/profile-photo/${idUSer}`, {
+            headers: { 'Content-Type': 'multipart/form-data', },
+        });
+        if (response.ok) {
+            const blob = await response.blob();
+            const imgURL = URL.createObjectURL(blob);
+            const img = document.getElementById("profile-photo");
+            img.src = imgURL;
+        } else {
+            console.error('Erro ao enviar o arquivo:', response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+    }
+}
