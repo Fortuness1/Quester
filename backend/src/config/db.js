39170a28554f18f00f068b563d1e10dbbd3ae6e8
Davi C. Pes. <davicalo2d@gmail.com>
2024-09-27 19:36:21 -0300

    m

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,23 +1,6 @@
 require('dotenv').config({ path: './backend/.env' });
 const client = process.env.MONGO_DB_URI;
-console.log(client)
 const mongoose = require("mongoose");
-
-// const connectToDatabase = async () => {
-//   await mongoose.connect(
-//     client,
-//     (error) => {
-//       if (error) {
-//         return console.log(
-//           "Ocorreu um erro ao se conectar com o banco de dados: ",
-//           error
-//         );
-//       }
-
-//       return console.log("Conexão ao banco de dados realizada com sucesso!");
-//     }
-//   );
-// };
 
 const connectToDatabase = async () => {
   await mongoose.connect(client).then(() => {
